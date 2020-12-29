085f5387b0403957cb1e48049491ffb880cee450
Sarveshwarneogi <sarveshwarneogi33@gmail.com>
2020-12-29 13:27:06 +0000

    added files

--- bankerroulette.py
+++ bankerroulette.py
@@ -1,9 +1,16 @@
-a=[""]
+import random
+a=[]
 print("enter a list of names:\n")
 c=0
-for i in a:
-  a=a.append(input("enter a name:"))
+k=1
+while(k!=0):
+  a.append(input("enter a name:"))
   c+=1
-  if a[c]=="1":
-    break
+  if (a[c-1]=="1"):
+    k=0
+a.pop()
 print(a)
+size=len(a)
+print(size)
+b=random.randint(1,5)
+print(f"{a[b]} pays the bill")
